096b182b14f90fad2c5597a2963c2a845fe3c479
Sameer Prashant Jadhav <63365882+sameer6699@users.noreply.github.com>
2024-06-22 10:43:28 +0530

    Time: 24 ms (12.5%), Space: 11.9 MB (21.36%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -15,18 +15,3 @@ print(solution.longestCommonPrefix(["dog", "racecar", "car"]))
 print(solution.longestCommonPrefix(["interspecies", "interstellar", "interstate"]))  
 print(solution.longestCommonPrefix(["throne", "dungeon"]))        
 print(solution.longestCommonPrefix(["throne", "throne"]))     
-
-#Solution of the Given Problem Statement 
-"""
-Explanation:
-1) Class Definition: The Solution class contains the longestCommonPrefix method.
-2) Method longestCommonPrefix: This method takes a list of strings strs and returns the longest common prefix.
-3) Initial Check: If strs is empty, return an empty string.
-4) Initial Prefix: Start by assuming the entire first string is the common prefix.
-5) Compare Each String: Iterate through the remaining strings in the list:
-    Use the find method to check if the current string starts with the prefix.
-    If the current string does not start with the prefix, shorten the prefix by one character and check again.
-    Repeat this process until a common prefix is found or the prefix becomes empty.
-6) Return the Result: After processing all strings, return the common prefix.
-
-"""
